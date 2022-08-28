fcecc5e22fa21bac9d94ff35752b3c9a7d2a9229
Jose Sebastian Toro Salas <Senjikaione@hotmail.com>
2022-08-27 22:01:18 -0400

    Se agrega Web API

---
+++ CL.TenedorYPalillos/TenedorYPalillos.WebAPI/Controllers/PingController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TenedorYPalillos.WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "Ping")]
+        public ActionResult<string> Get()
+        {
+            string url = Request.Scheme + "://" + Request.Host + "" + Request.Path + "" + Url.Content("~");
+
+            return url;
+        }
+
+    }
+}
